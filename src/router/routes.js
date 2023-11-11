e7ccd6863c84852ba9b05d69c468a770eeaac132
hadatullobabakulov28@gmail.cim <_@email.com>
2023-11-12 01:35:20 +0300

    f

--- src/router/routes.js
+++ src/router/routes.js
@@ -4,12 +4,14 @@ import PostIdPage from "../pages/PostIdPage";
 import Posts from "../pages/Posts";
 
 export const privateRoutes = [
+    {path: '/github.io/react_project', element: <Posts/>},
     {path: '/', element: <Posts/>},
     {path: '/about', element: <About/>},
     {path: '/posts/:id', element: <PostIdPage/>}
 ]
 
 export const publicRoutes = [
+    {path: '/github.io/react_project', element: <Login/>},
     {path: '/', element: <Login/>},
     {path: '/posts/:id', element: <Login/>},
     {path: '/about', element: <Login/>},
